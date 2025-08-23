a8a0a34cb4c7d327e7386346b9e5303740bc90f2
zhihengyi <zhihengyi@foxmail.com>
2025-08-23 18:28:17 +0800

    First Git

---
+++ 2.1.c
@@ -0,0 +1,15 @@
+/*  http://t.cn/E9WMRTE  */
+#include <stdio.h>
+
+int main(){
+	for(int a=0;a<=9;++a){
+		for(int b=0;b<=9;b++){
+			for(int c=0;c<=9;c++){
+				if(a*100+b*110+c*12==532){
+					printf("%d %d %d",a,b,c);
+				}
+			}
+		}
+	}
+	return 0;
+}
