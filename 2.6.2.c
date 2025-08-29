e73ecef084b8f86fa7166132cc87d162831c8cfc
Zhiheng Yi <zhihengyi@foxmail.com>
2025-08-29 20:03:17 +0800

    Delete 2.6.2.c

--- 2.6.2.c
+++
@@ -1,9 +0,0 @@
-/* http//t.cn/E9jXK5A */
-#include <stdio.h>
-
-int main() {
-    int y, m, d, c[]={0,31,59,90,120,151,181,212,243,273,304,334};
-    scanf("%d %d %d",&y,&m,&d);
-    printf("%d\n",c[m-1]+d+(m>2&&((y%4==0&&y%100!=0)||(y%400==0))));
-    return 0;
-}
